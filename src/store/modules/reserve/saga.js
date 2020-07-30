7ee37e9ec5099fd67b6bb7d314e7e6ca476bf7f6
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-07-30 18:24:19 -0300

    validation stock

--- src/store/modules/reserve/saga.js
+++ src/store/modules/reserve/saga.js
@@ -6,24 +6,45 @@ import api from '../../../services/api';
 // Esta funcão realiza uma requisição GET via Redux (tirando a necessidade de fazer a requisção toda hora do mesmo conteúdo);
 
 function* addToReserve({ id }){
-    const tripExist = yield select(
-        state => state.reserve.find(trip => trip.id === id )
-    ); 
-    if(tripExist){
-        const amount = tripExist.amount +1;
-        yield put(updateAmountReserve(id, amount));
-    }else{
-        const response = yield call(api.get, `/trips/${id}`);
-        const data = {
-            ...response.data,
-            amount: 1,
-        }; 
-        yield put(addReserveSuccess(data));
+    const tripExists = yield select(
+      state => state.reserve.find(trip => trip.id === id)
+    );
+  
+    const myStock = yield call(api.get, `/stock/${id}`);
+  
+    const stockValue = myStock.data.amount;
+    
+    const atualStock = tripExists ? tripExists.amount : 0; 
+  
+    const amount = atualStock + 1;
+  
+    if(amount > stockValue){
+      alert('Quantidade maxima atingida.');
+      return;
     }
-
-}  
+  
+    if(tripExists){ 
+  
+      yield put(updateAmountReserve(id, amount));
+  
+    }else{
+      const response = yield call(api.get, `trips/${id}` );
+  
+      const data = {
+        ...response.data,
+        amount: 1,
+      };
+  
+      yield put(addReserveSuccess(data));
+  
+      //history.push('/reservas');
+    }
+  
+  
+  
+  } 
 
 export default all([
     // Evita Várias requisicões ao mesmo tempo se clicar mais de uma vez no botão
     takeLatest('ADD_RESERVE_REQUEST', addToReserve)
-])
+]);
